927e7bf678c65625596a89abd43becea31bc2399
Szymon-Gut <kumunista12>
2025-06-03 22:41:30 +0200

    fix dataloader issue

--- src/dataloader.py
+++ src/dataloader.py
@@ -67,8 +67,7 @@ def get_cifar_dataloader(pkl_path: str, batch_size: int = 32, num_workers: int =
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
     test_transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.RandomResizedCrop(224, scale=(0.99, 1.0)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
